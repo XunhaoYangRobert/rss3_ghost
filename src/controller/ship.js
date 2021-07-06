f2d80cadf93cd6accd09f6ef81e8223dbd1f6645
TuziMoe <a@tuzi.moe>
2021-07-07 04:03:46 +1200

    support ipfs

--- src/controller/ship.js
+++ src/controller/ship.js
@@ -1,12 +1,15 @@
 const Base = require('./base.js');
 const RSS3 = require('rss3').default;
-
+const fs = require('fs');
+const createHTML = require('create-html');
+const axios = require('axios');
+const FormData = require('form-data');
 
 module.exports = class extends Base {
     async indexAction() {
         let pk = this.query("pk");
-        let type = this.query("type");
         let blog = this.post("post");
+        let filename = think.uuid('v4');
         let _rss3;
         try {
             _rss3 = new RSS3({
@@ -16,6 +19,23 @@ module.exports = class extends Base {
         } catch (err) {
             return this.fail(err);
         }
+        var html = createHTML({
+            title: blog.current.title,
+            body: blog.current.html
+        })
+        fs.writeFile('temp/' + filename + '.html', html, function (err) {
+            if (err) console.log(err)
+        });
+        var data = new FormData();
+        let htmlfile = fs.createReadStream('temp/' + filename + '.html')
+        data.append('file', htmlfile)
+        let postIpfs = await axios.post("https://ipfs.infura.io:5001/api/v0/add", data, {
+            headers: data.getHeaders(),
+            auth: {
+                username: "1uvdaOinj827yXYp9fIGqaXV759",
+                password: "51d876449baaeffb8fcc0d36e1b225e8"
+            },
+        });
         let postRSS3 = await _rss3.item.post({
             title: blog.current.title,
             tags: [
@@ -23,7 +43,7 @@ module.exports = class extends Base {
             ],
             summary: blog.current.plaintext.substring(0, 280),
             contents: [{
-                address: [blog.current.url],
+                address: ['https://ipfs.infura.io/ipfs/' + postIpfs.data.Hash],
                 mime_type: "text/html"
             }]
         });
